a6a6a3aa46a0d9e03ee86556d5808e38fc7bb91a
conorheins <conor.heins@gmail.com>
2021-06-07 11:53:57 +0200

    put 3way OU process parameters into config file

--- src/linear_diffusion_3d.py
+++ src/linear_diffusion_3d.py
@@ -1,11 +1,14 @@
 import os
-from .diffusions import LinearProcess
-from .utilities import initialize_random_friction_numpy, compute_FE_landscape, plot_hot_colourline
+from diffusions import LinearProcess
+from utilities import initialize_random_friction_numpy, compute_FE_landscape, plot_hot_colourline
 import jax.numpy as jnp
 from jax.numpy.linalg import inv, det, eigvals
 from jax import random
 import numpy as np 
 import matplotlib.pyplot as plt
+
+# import the 3way configuration variables 
+from configs.OU_3way import B, sigma, Pi, S, Q, b_mu, b_eta, sync, dimensions
 
 key = random.PRNGKey(0)
 
@@ -22,77 +25,10 @@ Setting up the steady-state
 
 n_var = 3         # dimensionality
 
-eta_dim = [0]
-b_dim = [1]
-mu_dim = [2]
-pi_dim = b_dim + mu_dim # particular states
-
-# Define precision
-Pi = jnp.array([ [3., 1., 0.], 
-                [1., 3., 0.5], 
-                [0., 0.5, 2.5] ]) #selected precision matrix
-
-# enforce symmetric
-Pi = (Pi + Pi.T) / 2.0
-
-# Ensure Pi is positive definite
-while (eigvals(Pi) <= 0).any():
-    Pi -= 2 * jnp.min(eigvals(Pi)) * jnp.eye(n_var)
-
-# Compute the stationary covariance
-S = inv(Pi)
-
-'''
-Setting up synchronisation map
-'''
-
-# Check that the synchronisation map is well defined
-assert S[mu_dim, b_dim] != 0, "Synchronisation map not well defined: bold_mu(b) not invertible!"
-
-# define the linear synchronisation map
-b_mu = S[mu_dim, b_dim] * S[b_dim, b_dim] ** (-1)  # expected internal state
-b_eta = S[eta_dim, b_dim] * S[b_dim, b_dim] ** (-1)  # expected external state
-sync = Pi[eta_dim, eta_dim] ** (-1) * Pi[eta_dim, b_dim] * Pi[mu_dim, b_dim] ** (-1) * Pi[mu_dim, mu_dim]  # sync map
-
-'''
-Setting up the OU process
-'''
-
-# volatility
-sigma = jnp.array([ [2, 1.5, 0.5],
-                    [0., 3., 2.],
-                    [0., 0., 2.] ]) #selected non-degenerate noise
-
-# see whether noise is degenerate or not
-print(f'det sigma = {det(sigma)}')
-
-#diffusion tensor
-D = sigma @ sigma.T / 2  # diffusion tensor
-
-# solenoidal flow
-Q = np.array([ [0., 1.5, 0.5],
-                [-1.5, 0., -1.],
-                [-0.5, 1., 0.] ]) #selected solenoidal flow
-
-Q = (Q - Q.T)/2 # ensure anti-symmetry
-
-# Drift matrix
-B = (D + Q) @ Pi  # drift matrix
-
-if (eigvals(B) <= -1e-5).any():
-    print(eigvals(B))
-    raise TypeError("Drift should have non-negative spectrum")
-    
-# 1) We check it solves the Sylvester equation: BS + SB.T = 2D
-assert jnp.allclose(B @ S + S @ B.T, 2 * D), "Sylvester equation not solved"
-
-# 2) we check that there are no numerical errors due to ill conditioning
-assert jnp.allclose(inv(S), Pi), "Precision and inverse covariance are different"
-
-# We check that the stationary covariance is indeed positive definite
-if (eigvals(S) <= 0).any():
-    print(eigvals(S))
-    raise TypeError("Stationary covariance not positive definite")
+eta_dim = dimensions['eta']
+b_dim = dimensions['b']
+mu_dim = dimensions['mu']
+pi_dim = dimensions['pi']
 
 # Setting up the OU process
 process = LinearProcess(dim=n_var, friction=B, volatility=sigma)  # create process
@@ -105,7 +41,7 @@ OU process steady-state simulation
 #all subsequent simulations can only be trusted to the extent that the synchronisation map
 # works in the steady-state simulation
 
-dt, T, n_real = 0.01, 500, 1000 # duration of single time-step, total number of integration timesteps, total number of parallel realizations
+dt, T, n_real = 0.01, 500, 1000 # duration of single time-step, total number of integration timesteps, number of parallel paths to simulate
 
 # initialize process starting state
 x0 = jnp.transpose(random.multivariate_normal(key, jnp.zeros(n_var), S, shape = (n_real,) ), (1, 0))
@@ -128,20 +64,20 @@ bin_idx_b = np.digitize(b_hist, bin_edges, right=True)
 bin_counts = np.histogram(b_hist, bin_edges)[0]
 bin_centers = bin_edges[:-1] + 0.5*np.diff(bin_edges)
 
+unique_bins, unique_bin_occurrences = np.unique(bin_idx_b, return_inverse = True)
+
 # Calculate empirical average of internal and external states, given particular occurrences of a 'bin' of blanket states
 
 mu_cond_b = np.empty_like(bin_centers)
 eta_cond_b = np.empty_like(bin_centers)
 
-unique_bins, unique_bin_occurrences = np.unique(bin_idx_b, return_inverse = True)
-
-mu_hist = x_for_histogram[:,mu_dim].squeeze()
-eta_hist = x_for_histogram[:,eta_dim].squeeze()
+mu_binned = x_for_histogram[:,mu_dim].squeeze()
+eta_binned = x_for_histogram[:,eta_dim].squeeze()
 
 for unique_bin_idx in range(len(unique_bins)):
 
-    mu_cond_b[unique_bins[unique_bin_idx]-1] = mu_hist[unique_bin_occurrences == unique_bin_idx].mean()
-    eta_cond_b[unique_bins[unique_bin_idx]-1] = eta_hist[unique_bin_occurrences == unique_bin_idx].mean()
+    mu_cond_b[unique_bins[unique_bin_idx]-1] = mu_binned[unique_bin_occurrences == unique_bin_idx].mean()
+    eta_cond_b[unique_bins[unique_bin_idx]-1] = eta_binned[unique_bin_occurrences == unique_bin_idx].mean()
 
 # map the empirical most likely internal state to a corresponding expected external state, via the synchronization map
 sync_boldmu = sync * mu_cond_b
@@ -152,7 +88,6 @@ plt.scatter(bin_centers[bin_counts > 1000], sync_boldmu[bin_counts > 1000], s=1,
 plt.scatter(bin_centers[bin_counts > 1000], eta_cond_b[bin_counts > 1000], s=1, alpha=0.5, label='Actual: $\mathbf{\eta}(b_t)$')
 plt.xlabel('Blanket state space $\mathcal{B}$')
 plt.ylabel('External state space $\mathcal{E}$')
-# I don't think plotting the Pearson correlation is particularly informative here - the correlation coefficient is typically used in a statistical inference / EDA setting to actual determine the extent of a relationship
 # cor = scipy.stats.pearsonr(sync_bold_mu[j == 1], bold_eta_empirical[j == 1])
 # plt.title(f'Pearson correlation = {np.round(cor[0], 6)}...')
 plt.legend(loc='upper right')
@@ -165,7 +100,7 @@ OU process perturbed simulation
 
 _, key = random.split(key)
 
-b = 10.0 # specify blanket state
+b = 10. # specify perturbed blanket state
 
 b_init = b * jnp.ones( n_real)
 
@@ -191,7 +126,7 @@ blanket = jnp.linspace(jnp.min(x[:, b_dim, :]) - 1, jnp.max(x[:, b_dim, :]) + 0.
 
 F_landscape = compute_FE_landscape(blanket, internal, b_eta, sync, S_part_inv)
 
-realisation_idx = 10  # which sample path to show (between 0 and N)
+realisation_idx = 3  # which sample path to show (between 0 and N)
 
 plt.figure(2)
 plt.title('Free energy $F(b, \mu)$')
